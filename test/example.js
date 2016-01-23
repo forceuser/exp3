b98fa99733fe07eb04b1a7c7d6c4a847f7a86e48
Виталий Дуб <forceuserz@gmail.com>
2016-01-23 09:39:57 +0000

    pass tests

--- test/example.js
+++ test/example.js
@@ -3,4 +3,5 @@ const f = require('../index');
 
 test('test1', t => {
 	t.is(f(true), true);
+	t.is(f(false), false);
 });
--- test/path.js
+++ test/path.js
@@ -1,8 +1,8 @@
 // const resolve = require("../npm-resolve");
 // const request = require("../gen-request");
-const test = require("ava");
+const test = require('ava');
 
 /** This is a description of the foo function. */
-test("Request generation test", t => {
-    t.is(true,true);
+test('Request generation test', t => {
+	t.is(true, true);
 });
